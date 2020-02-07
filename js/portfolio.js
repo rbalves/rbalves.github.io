f79cf7e85001417f63c784330100212601a1a049
Rafael Alves <rbalves.ads@gmail.com>
2020-02-06 21:31:21 -0400

    Add badges tecnologies in portfolio github

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,6 +13,47 @@ const getRepos = () => {
 
 		orderReposByStars(repos);
 
+		const allTecnologies = repos.map(repo => {
+			const {language} = repo;
+			return language;
+		});
+
+		const duplicatedTecnologiesRemoved = allTecnologies.filter((a, b) => allTecnologies.indexOf(a) === b);
+
+		const tecnologies = [];
+
+		duplicatedTecnologiesRemoved.forEach(item => {
+			const countTecnologie = allTecnologies.filter(tecnologie => item == tecnologie).length;
+			
+			const tecnologieInfo = {
+				description: item || 'Outros',
+				qty: countTecnologie
+			}
+
+			tecnologies.push(tecnologieInfo);
+		});
+
+		const ul = document.createElement('ul');
+		
+		ul.style.listStyleType = 'none';
+		ul.style.margin = '0';
+		ul.style.padding = '0';
+
+		tecnologies.forEach(tecnologie => {
+			const li = document.createElement('li');
+			li.innerHTML = `${tecnologie.description} (${tecnologie.qty})`;
+			li.style.display = 'inline';
+			li.style.background = '#222d32';
+			li.style.color = '#fff';
+			li.style.margin = '5px'
+			li.style.padding = '3px';
+			li.style.borderRadius = '4px';
+			ul.appendChild(li);
+		});
+
+		const divBadges = document.getElementById('badges-tecnologies');
+		divBadges.appendChild(ul);
+
 		repos.forEach(repo => {
 
 			const {name, description, created_at, language, stargazers_count, html_url} = repo;
@@ -20,13 +61,14 @@ const getRepos = () => {
 			const divCard = document.createElement("div");
 			divCard.classList.add('card');
 			divCard.style.padding = '2%';
+			divCard.style.marginTop = '1%';
 
 			const divCardBody = document.createElement("div");
 			divCardBody.classList.add('card-body');
 
 			divCard.appendChild(addText('h4', 'card-title', name || 'Sem nome'));
 			divCard.appendChild(addText('strong', 'card-text', description || 'Nenhuma descrição'));
-			divCard.appendChild(addText('p', 'card-text', 'Criado em: ' + (created_at.substr(0,10)) || 'Não identificada' ));
+			divCard.appendChild(addText('p', 'card-text', 'Criado em: ' + (created_at.substr(0,10)) || 'Data não identificada' ));
 			divCard.appendChild(addText('p', 'card-text', 'Linguagem: ' + (language || 'Não identificada')));
 			divCard.appendChild(addText('p', 'card-text', 'Estrelas: ' + stargazers_count));
 
@@ -40,7 +82,7 @@ const getRepos = () => {
 		})
 	})
 	.catch(error => {
-		divRepos.appendChild(document.createTextNode('Erro ao consultar a API do Github.'));
+		divRepos.appendChild(document.createTextNode('Desculpe! Ocorreu um erro.'));
 	})
 }
 
